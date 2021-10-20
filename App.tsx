1358717a6440aa87ffa867aba6682cad456c4559
Talles Corradini <tallescorradini@gmail.com>
2021-10-20 19:41:51 -0300

    feat: add project fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,21 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { Home } from "./src/screens/Home";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import AppLoading from "expo-app-loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return <Home />;
 }
