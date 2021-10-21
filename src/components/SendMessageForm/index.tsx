bea1e2f18ad24994f67be1238bcaf8aef7451723
Talles Corradini <tallescorradini@gmail.com>
2021-10-20 22:47:18 -0300

    fix: move send message form  up when keyboard appears

--- src/components/SendMessageForm/index.tsx
+++ src/components/SendMessageForm/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { TextInput, View } from "react-native";
+import { TextInput, KeyboardAvoidingView, View } from "react-native";
 
 import { styles } from "./styles";
 import { COLORS } from "../../theme";
@@ -10,22 +10,24 @@ export function SendMessageForm() {
   const [sendingMessage, setSendingMessage] = useState(false);
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        keyboardAppearance="dark"
-        placeholder="Qual sua expectativa para o evento?"
-        placeholderTextColor={COLORS.GRAY_PRIMARY}
-        multiline
-        maxLength={140}
-        onChangeText={setMessage}
-        editable={!sendingMessage}
-        style={styles.input}
-      />
-      <Button
-        title="ENVIAR MENSAGEM"
-        backgroundColor={COLORS.PINK}
-        color={COLORS.WHITE}
-      />
-    </View>
+    <KeyboardAvoidingView behavior="position">
+      <View style={styles.container}>
+        <TextInput
+          keyboardAppearance="dark"
+          placeholder="Qual sua expectativa para o evento?"
+          placeholderTextColor={COLORS.GRAY_PRIMARY}
+          multiline
+          maxLength={140}
+          onChangeText={setMessage}
+          editable={!sendingMessage}
+          style={styles.input}
+        />
+        <Button
+          title="ENVIAR MENSAGEM"
+          backgroundColor={COLORS.PINK}
+          color={COLORS.WHITE}
+        />
+      </View>
+    </KeyboardAvoidingView>
   );
 }
