9440cd3fed6474684209344c7fc97bb75d06ad08
Talles Corradini <tallescorradini@gmail.com>
2021-10-21 14:40:09 -0300

    feat: fetch messages from backend

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ScrollView } from "react-native";
-import { Message } from "../Message";
+import { api } from "../../services/api";
+import { Message, MessageProps } from "../Message";
 
 import { styles } from "./styles";
 
@@ -14,23 +15,26 @@ const message = {
 };
 
 export function MessageList() {
+  const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([]);
+
+  useEffect(() => {
+    async function fetchMessages() {
+      const messagesResponse = await api.get<MessageProps[]>("/messages/last3");
+      setCurrentMessages(messagesResponse.data);
+    }
+
+    fetchMessages();
+  }, []);
+
   return (
     <ScrollView
       style={styles.container}
       contentContainerStyle={styles.content}
       keyboardShouldPersistTaps="never"
     >
-      <Message data={message} />
-      <Message data={message} />
-      <Message data={message} />
-      <Message data={message} />
-      <Message data={message} />
-      <Message data={message} />
-      <Message data={message} />
-      <Message data={message} />
-      <Message data={message} />
-      <Message data={message} />
-      <Message data={message} />
+      {currentMessages.map((message) => (
+        <Message key={message.id} data={message} />
+      ))}
     </ScrollView>
   );
 }
