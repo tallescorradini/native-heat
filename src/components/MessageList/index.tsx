7aaf1224d3587d6fc473f256f00873d4a33cb089
Talles Corradini <tallescorradini@gmail.com>
2021-10-21 15:37:23 -0300

    feat: fetch messages from backend using socket.io

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,18 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { ScrollView } from "react-native";
+import { io } from "socket.io-client";
 import { api } from "../../services/api";
 import { Message, MessageProps } from "../Message";
 
 import { styles } from "./styles";
 
-const message = {
-  id: "1",
-  text: "Mensagem de teste",
-  user: {
-    name: "Talles",
-    avatar_url: "https://github.com/tallescorradini.png",
-  },
-};
+let messagesQueue: MessageProps[] = [];
+
+const socket = io(String(api.defaults.baseURL));
+socket.on("new_message", (newMessage) => {
+  messagesQueue.push(newMessage);
+});
 
 export function MessageList() {
   const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([]);
@@ -24,6 +23,21 @@ export function MessageList() {
     }
 
     fetchMessages();
+  }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setCurrentMessages((prevState) => [
+          messagesQueue[0],
+          prevState[0],
+          prevState[1],
+        ]);
+        messagesQueue.shift();
+      }
+    });
+
+    return () => clearInterval(timer);
   }, []);
 
   return (
