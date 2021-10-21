f0839ff784311596e6acd858cb8108a2fe20ea63
Talles Corradini <tallescorradini@gmail.com>
2021-10-20 22:45:02 -0300

    feat: add send message form component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text } from "react-native";
 import { Header } from "../../components/Header";
 import { MessageList } from "../../components/MessageList";
+import { SendMessageForm } from "../../components/SendMessageForm";
 import { SignInBox } from "../../components/SignInBox";
 
 import { styles } from "./styles";
@@ -12,7 +13,7 @@ function Home() {
       <Header />
       <MessageList />
 
-      <SignInBox />
+      <SendMessageForm />
     </View>
   );
 }
