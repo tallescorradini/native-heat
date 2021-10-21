bd902d037a24a2ca20e62a6a75936649d789b36c
Talles Corradini <tallescorradini@gmail.com>
2021-10-20 21:30:04 -0300

    feat: add message list and message components

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { Header } from "../../components/Header";
+import { MessageList } from "../../components/MessageList";
 
 import { styles } from "./styles";
 
@@ -8,6 +9,7 @@ function Home() {
   return (
     <View style={styles.container}>
       <Header />
+      <MessageList />
     </View>
   );
 }
