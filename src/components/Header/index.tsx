b35b402f89ee0d8d8aa422d3f4fbf9d863d53a36
Talles Corradini <tallescorradini@gmail.com>
2021-10-21 12:58:51 -0300

    feat: add dynamic user data to Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,8 +4,11 @@ import { Text, View, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 import LogoSvg from "../../assets/logo.svg";
 import { UserPhoto } from "../UserPhoto";
+import { useAuth } from "../../hooks/auth";
 
 export function Header() {
+  const { user } = useAuth();
+
   return (
     <View style={styles.container}>
       <LogoSvg />
@@ -14,7 +17,7 @@ export function Header() {
         <TouchableOpacity>
           <Text style={styles.logoutText}>Sair</Text>
         </TouchableOpacity>
-        <UserPhoto imageUri="https://github.com/tallescorradini.png" />
+        <UserPhoto imageUri={user?.avatar_url} />
       </View>
     </View>
   );
