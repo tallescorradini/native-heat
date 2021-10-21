c52f2755856e685b6185bba2e85abe97201201c4
Talles Corradini <tallescorradini@gmail.com>
2021-10-21 14:59:46 -0300

    feat: implement send message functionality

--- src/components/SendMessageForm/index.tsx
+++ src/components/SendMessageForm/index.tsx
@@ -1,14 +1,36 @@
 import React, { useState } from "react";
-import { TextInput, KeyboardAvoidingView, View } from "react-native";
+import {
+  TextInput,
+  KeyboardAvoidingView,
+  View,
+  Alert,
+  Keyboard,
+} from "react-native";
 
 import { styles } from "./styles";
 import { COLORS } from "../../theme";
 import { Button } from "../Button";
+import { api } from "../../services/api";
 
 export function SendMessageForm() {
   const [message, setMessage] = useState("");
   const [sendingMessage, setSendingMessage] = useState(false);
 
+  async function handleMessageSubmit() {
+    const messageFormatted = message.trim();
+
+    if (messageFormatted.length > 0) {
+      setSendingMessage(true);
+      await api.post("/messages", { message: messageFormatted });
+
+      setMessage("");
+      Keyboard.dismiss();
+      setSendingMessage(false);
+      Alert.alert("Mensagem enviada com sucesso!");
+    } else {
+      Alert.alert("Escreva a mensagem para enviar");
+    }
+  }
   return (
     <KeyboardAvoidingView behavior="position">
       <View style={styles.container}>
@@ -18,6 +40,7 @@ export function SendMessageForm() {
           placeholderTextColor={COLORS.GRAY_PRIMARY}
           multiline
           maxLength={140}
+          value={message}
           onChangeText={setMessage}
           editable={!sendingMessage}
           style={styles.input}
@@ -26,6 +49,8 @@ export function SendMessageForm() {
           title="ENVIAR MENSAGEM"
           backgroundColor={COLORS.PINK}
           color={COLORS.WHITE}
+          isLoading={sendingMessage}
+          onPress={handleMessageSubmit}
         />
       </View>
     </KeyboardAvoidingView>
