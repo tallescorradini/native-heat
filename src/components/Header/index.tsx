d449bb1a48da5b2847bb1599e351363904f2b035
Talles Corradini <tallescorradini@gmail.com>
2021-10-20 21:10:17 -0300

    feat: add UserPhoto to Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,14 +3,19 @@ import { Text, View, TouchableOpacity } from "react-native";
 
 import { styles } from "./styles";
 import LogoSvg from "../../assets/logo.svg";
+import { UserPhoto } from "../UserPhoto";
 
 export function Header() {
   return (
     <View style={styles.container}>
       <LogoSvg />
-      <TouchableOpacity>
-        <Text style={styles.logoutText}>Sair</Text>
-      </TouchableOpacity>
+
+      <View style={styles.logoutButton}>
+        <TouchableOpacity>
+          <Text style={styles.logoutText}>Sair</Text>
+        </TouchableOpacity>
+        <UserPhoto imageUri="https://github.com/tallescorradini.png" />
+      </View>
     </View>
   );
 }
